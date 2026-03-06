59edeacbff3bc596cd335a9fbb6202bb7d1bd891
Miguel <migromosu@gmail.com>
2026-03-06 17:39:52 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const boton = document.getElementById("botonSaludo");
-const mensaje = document.getElementById("mensaje");
-
-boton.addEventListener("click", function () {
-  mensaje.textContent = "Funciona. Ya has creado una mini app dentro de GitHub.";
-});
